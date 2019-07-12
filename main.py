579115ebe76448ca54687f8525edd5939d33fb1e
seaneightysix <sean.eightysix@GMAIL.com>
2019-07-12 06:02:47 -0500

    update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donor, Donation 
 
 app = Flask(__name__)
 
@@ -15,9 +15,27 @@ def home():
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+
+    #if 'username' not in session:
+        #return redirect(url_for('login'))
+
+    donors = Donor.select()
+
+    if request.method == 'POST':
+        donation = Donation(value=request.form['donation'])
+        donation.donor = donor=request.form['donor']
+        donation.save()
+
+        return redirect(url_for('create'))
+
+    else:
+        return render_template('create.jinja2', donors=donors)
     
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 6738))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
